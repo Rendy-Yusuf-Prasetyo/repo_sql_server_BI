0b44c98a7784e57d582f1fa01a17156ea4122a32
Rendy Yusuf Prasetyo <43400387+Rendy-Yusuf-Prasetyo@users.noreply.github.com>
2021-07-12 17:41:26 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -13,3 +13,13 @@ dim_pelanggan AS c ON a.id_pelanggan = c.id_pelanggan INNER JOIN
 dim_waktu AS d ON a.id_waktu = d.id_waktu
 JOIN dim_waktu AS e ON a.id_waktu = e.id_waktu
 GROUP BY b.nama_barang, a.quantity, e.bulan
+
+-- PERCOBAAN
+
+select SUM(a.quantity * b.harga_barang) "PEMBELIAN BERSIH",
+SUM(c.stok * b.harga_barang) "PERSEDIAAN AWAL",
+SUM((c.stok * b.harga_barang) - ((c.stok - a.quantity) * b.harga_barang)) "PERSEDIAAN AKHIR"
+from fakta_pengeluaran a 
+JOIN dim_barang b ON a.ID_BARANG = b.iD_BARANG
+JOIN dim_kain c ON a.ID_KAIN = c.ID_KAIN
+JOIN dim_waktu d ON a.id_waktu = d.id_waktu
